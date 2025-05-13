b232e7989659ca4b25fe958102e7d79296544491
John Doe <johndoe@example.com>
2025-05-13 22:16:44 +0800

    Update init_PrintKeycodes.lua

--- MacAppleScripts/Hammerspoon/init_PrintKeycodes.lua
+++ MacAppleScripts/Hammerspoon/init_PrintKeycodes.lua
@@ -1,3 +1,11 @@
 hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(e)
   print("KeyCode: " .. e:getKeyCode())
 end):start()
+
+
+-- Create an eventtap to listen to key presses
+-- hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
+--     local keyCode = event:getKeyCode()
+--     local keyName = hs.keycodes.map[keyCode] or "Unknown Key"
+--     print("Key pressed: " .. keyName .. " (KeyCode: " .. keyCode .. ")")
+-- end):start()
